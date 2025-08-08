168cdc9490b21ad4e0d5681503ce78ce2df85645
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-08-08 21:32:04 +0530

    Refactor ContentBox component to replace Tabs with TabContainer for improved structure and maintainability

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,9 +1,6 @@
 import type { IPInfo, CountryInfo } from "../types/ApiResponse";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
-import { PDFDownloadLink } from "@react-pdf/renderer";
-import MyDocument from "./CreatePDF";
+import TabContainer from "./TabContainer";
 
 type ContentBoxProps = {
   content: IPInfo;
@@ -13,7 +10,7 @@ type ContentBoxProps = {
 const ContentBox = ({ content, country }: ContentBoxProps) => {
   if (!content.ip && !content.country_name)
     return (
-      <div className="flex items-center justify-center h-96">
+      <div className="flex items-center justify-center h-96 max-sm:text-sm">
         <p>
           Please enable trackers in your browser for this application to work.
         </p>
@@ -48,175 +45,7 @@ const ContentBox = ({ content, country }: ContentBoxProps) => {
       </div>
       {/* IP Details */}
       <div className="w-1/3 bg-white/7 rounded-xl p-4 max-lg:w-full">
-        <Tabs defaultValue="general" className="">
-          <TabsList>
-            <TabsTrigger value="general">General</TabsTrigger>
-            <TabsTrigger value="details">Details</TabsTrigger>
-            <TabsTrigger value="download-print-info">
-              Download Info
-            </TabsTrigger>
-          </TabsList>
-
-          {/* General Tab */}
-          <TabsContent
-            value="general"
-            className="grid grid-cols-2 gap-4 w-full"
-          >
-            <Card className="bg-white/10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Continent</CardTitle>
-                <CardDescription className="text-lg">
-                  {country.region} ({content.continent_code})
-                </CardDescription>
-              </CardHeader>
-            </Card>
-            <Card className="bg-white/10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Country</CardTitle>
-                <CardDescription className="text-lg">
-                  {content!.country_name}
-                </CardDescription>
-              </CardHeader>
-            </Card>
-            <Card className="bg-white/10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Region</CardTitle>
-                <CardDescription className="text-lg">
-                  {content!.region}
-                </CardDescription>
-              </CardHeader>
-            </Card>
-            <Card className="bg-white/10">
-              <CardHeader>
-                <CardTitle className="text-2xl">City</CardTitle>
-                <CardDescription className="text-lg">
-                  {content!.city}
-                </CardDescription>
-              </CardHeader>
-            </Card>
-            <Card className="bg-white/10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Subregion</CardTitle>
-                <CardDescription className="text-lg">
-                  {country.subregion}
-                </CardDescription>
-              </CardHeader>
-            </Card>
-            <Card className="bg-white/10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Flag</CardTitle>
-                <CardDescription className="text-lg">
-                  <img
-                    src={country.flags.svg}
-                    alt="National Flag"
-                    className="w-26"
-                  />
-                </CardDescription>
-              </CardHeader>
-            </Card>
-          </TabsContent>
-
-          {/* Details Tab */}
-          <TabsContent value="details">
-            <div className="p-2 grid grid-cols-2 gap-4 w-full h-[500px] overflow-y-auto">
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">IP Address</CardTitle>
-                  <CardDescription className="text-lg">
-                    {content.ip}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">ISP</CardTitle>
-                  <CardDescription className="text-lg">
-                    {content.org}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Coordinate</CardTitle>
-                  <CardDescription className="text-lg">
-                    {content.longitude}, {content.latitude}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Timezone</CardTitle>
-                  <CardDescription className="text-lg">
-                    {content.timezone}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Postal Code</CardTitle>
-                  <CardDescription className="text-lg">
-                    {content.postal}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Country Code</CardTitle>
-                  <CardDescription className="text-lg">
-                    {content.country_code}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Region Code</CardTitle>
-                  <CardDescription className="text-lg">
-                    {content.region_code}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Currency</CardTitle>
-                  <CardDescription className="text-lg">
-                    {country.currencies
-                      ? Object.keys(country.currencies).join(", ")
-                      : "N/A"}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Population</CardTitle>
-                  <CardDescription className="text-lg">
-                    {country.population?.toLocaleString() || "N/A"}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-              <Card className="bg-white/10">
-                <CardHeader>
-                  <CardTitle className="text-2xl">Capital</CardTitle>
-                  <CardDescription className="text-lg">
-                    {country.capital?.[0] || "N/A"}
-                  </CardDescription>
-                </CardHeader>
-              </Card>
-            </div>
-          </TabsContent>
-
-          {/* Download & Print Tab */}
-          <TabsContent value="download-print-info">
-            <div className="flex gap-4 mt-10">
-              <PDFDownloadLink
-                className="cursor-pointer bg-black text-white dark:bg-white/95 font-semibold text-sm flex items-center justify-center dark:text-black py-1 px-4 rounded-md"
-                document={<MyDocument content={content} country={country} />}
-                fileName={`IP-Data-${content.ip}.pdf`}
-              >
-                Download Info (pdf)
-              </PDFDownloadLink>
-            </div>
-          </TabsContent>
-        </Tabs>
+        <TabContainer content={content} country={country} />
       </div>
     </section>
   );
