702e678ce39bfbc4c5328d31c1ec3f5dba3ba886
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-08-08 21:33:03 +0530

    Update heading size in App component for better responsiveness and adjust Input component formatting

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ const App = () => {
   return (
     <main className="max-w-[90rem] mx-auto p-4">
       <Navbar />
-      <h1 className="text-4xl text-center mt-6">
+      <h1 className="text-2xl md:text-4xl text-center mt-6">
         Track any <span className="font-bold text-green-400">IP</span> and
          <span className="font-bold text-yellow-400"> discover </span> <br /> useful information.
       </h1>
-      <Input setContent={setContent} setCountry={setCountry} />
+      <Input setContent={setContent} setCountry={setCountry}/>
       <IpContent content={content} country={country!} />
     </main>
   );
